4aec917cef5d07e4f9dc6b961a63f76a8a36b6e4
Karolina Koza <>
2022-01-12 19:44:18 +0000

    Improved UX

--- main.py
+++ main.py
@@ -1,63 +1,9 @@
 import random
+import hangman_art
+import hangman_words
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
+stages = hangman_art.stages
+word_list = hangman_words.word_list
 clue=random.choice(word_list)
 guessed=['_'for x in clue]
 theend=False
@@ -65,13 +11,15 @@ win=False
 lose=False
 mistakes=0
 maxmistakes=len(stages)
+tested_letters=[]
+
+print(hangman_art.logo, '\nWelcome to the Hangman The Game.\nHave fun!')
 print(guessed)
-
-print('Welcom to the Hangman the game.\nHave fun!')
-
 while not (win or lose):
   answer=input('Guess a letter: ').lower()
   changes=0
+  tested_letters+=answer
+
   for number, letter in enumerate(clue):
     if letter==answer:
       guessed[number]=answer
@@ -82,7 +30,7 @@ while not (win or lose):
   if mistakes>0:
     print(stages[-mistakes])
     
-  print(guessed)
+  print(guessed,f'\nUsed letters:{tested_letters}')
 
   if not '_' in guessed:
     win=True
@@ -90,5 +38,6 @@ while not (win or lose):
     lose=True
 if win==True:
   print('Congratulations, you won a game!')
-  else:
-    print('Maybe next time.')
+else:
+  print('Maybe next time you will win.')
+print(f'The answer was: {clue}')
