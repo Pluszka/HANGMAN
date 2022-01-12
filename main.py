131fdba36950df58cca2a8cfa06db549fe442c1b
Karolina Koza <>
2022-01-12 18:06:30 +0000

    counting mistakes

--- main.py
+++ main.py
@@ -1,11 +1,88 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 clue=random.choice(word_list)
-answer=input('Guess a letter: ').lower()
-
 guessed=['_'for x in clue]
-for number, letter in enumerate(clue):
-  if letter==answer:
-    guessed[number]=answer
+theend=False
+win=False
+lose=False
+mistakes=0
+maxmistakes=len(stages)
 print(guessed)
+
+while not (win or lose):
+  answer=input('Guess a letter: ').lower()
+  changes=0
+  for number, letter in enumerate(clue):
+    if letter==answer:
+      guessed[number]=answer
+      changes+=1
+  if changes==0:
+    mistakes+=1
+
+  if mistakes>0:
+    print(stages[-mistakes])
+    
+  print(guessed)
+
+  if not '_' in guessed:
+    win=True
+  if mistakes==maxmistakes:
+    lose=True
